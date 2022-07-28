0e466607061d8f25f3dcbc9e9ebbed9175f3ce7e
ZeroBug <404.zerobug@gmail.com>
2022-07-29 00:36:26 +0600

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h2>Receipt Details</h2>
+      <form className="add_form">
+        <div>
+          <p>Date</p>
+          <p>Amount</p>
+          <p>Payment Mode</p>
+          <p>Remark</p>
+        </div>
+        <div className="form_inputs">
+          <input
+            type="date"
+            name="date"
+            required="required"
+            placeholder="Enter Date"
+          />
+          <input
+            type="number"
+            name="amount"
+            required="required"
+            placeholder="Enter Amount (in INR)"
+          />
+          <input
+            type="text"
+            name="phoneNumber"
+            required="required"
+            placeholder="Enter a phone number..."
+          />
+          <input
+            type="email"
+            name="email"
+            required="required"
+            placeholder="Enter remark."
+          />
+        </div>
+
+        <div>
+          <button type="submit">CANCEL</button>
+          <button type="submit">SUBMIT</button>
+        </div>
+      </form>
     </div>
   );
 }
